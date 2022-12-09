57b5a92ba40056942a4b1e9deaf41f5151c75800
Matthew Fahrenkrug <matthew.fahrenkrug@starlab.io>
2022-12-09 17:02:19 -0500

    adc part 2

--- src/adc_instruction.rs
+++ src/adc_instruction.rs
@@ -1,17 +1,70 @@
 use capstone::arch::x86::{X86InsnDetail, X86OperandType};
 use capstone::prelude::DetailsArchInsn;
 use itertools::Itertools;
-use crate::memory_operand::{MemoryOperandOrRegister8, MemoryOperandOrRegister16, MemoryOperandOrRegister32, MemoryOperandOrRegister64};
+use crate::memory_operand::{MemoryOperandOrRegister8, MemoryOperandOrRegister16, MemoryOperandOrRegister32, MemoryOperandOrRegister64, MemoryOperand};
 use crate::registers::{OperandSize, Register8, Register16, Register32, Register64};
+use crate::utils::{imm_i8, imm_i16, imm_i32};
 
 //https://www.felixcloutier.com/x86/adc
 #[derive(Debug, PartialEq)]
 pub enum AdcInstruction{
-    R8Rm8{
-        target: MemoryOperandOrRegister8,
-        to_add: Register8
+    ALimm8 {
+        imm8: i8
     },
-    R8dRm8d{
+    AXimm16 {
+        imm8: i16
+    },
+    EAXimm32 {
+        imm16: i32
+    },
+    RAXimm32 {
+        imm32: i32
+    },
+    Rm8imm8 {
+        target: MemoryOperandOrRegister8,
+        imm8: i8,
+    },
+    Rm16imm16 {
+        target: MemoryOperandOrRegister16,
+        imm16: i16,
+    },
+    Rm32imm32 {
+        target: MemoryOperandOrRegister32,
+        imm32: i32,
+    },
+    Rm64imm32 {
+        target: MemoryOperandOrRegister64,
+        imm32: i32,
+    },
+    Rm16imm8 {
+        target: MemoryOperandOrRegister16,
+        imm8: i8,
+    },
+    Rm32imm8 {
+        target: MemoryOperandOrRegister32,
+        imm8: i8,
+    },
+    Rm64imm8 {
+        target: MemoryOperandOrRegister64,
+        imm8: i8,
+    },
+    Rm8R8{
+        target: Register8,
+        to_add: MemoryOperandOrRegister8,
+    },
+    Rm16R16{
+        target: Register16,
+        to_add: MemoryOperandOrRegister16,
+    },
+    Rm32R32{
+        target: Register32,
+        to_add: MemoryOperandOrRegister32,
+    },
+    Rm64R64{
+        target: Register64,
+        to_add: MemoryOperandOrRegister64,
+    },
+    R8Rm8{
         target: MemoryOperandOrRegister8,
         to_add: Register8
     },
@@ -46,10 +99,25 @@ impl AdcInstruction {
                                 let to_add = Register64::new(*reg_id);
                                 AdcInstruction::R64Rm64 { target, to_add }
                             }
-                            X86OperandType::Imm(_) => {
-                                todo!()
+                            X86OperandType::Imm(imm) => {
+                                match OperandSize::from_capstone_size(operands[1].size) {
+                                    OperandSize::QuadWord => {
+                                        AdcInstruction::Rm64imm32 { target, imm32: imm_i32(*imm) }
+                                    }
+                                    OperandSize::DoubleWord |
+                                    OperandSize::Word => {
+                                        panic!("Unexpected constant size")
+                                    }
+                                    OperandSize::HalfWord => {
+                                        AdcInstruction::Rm64imm8 { target, imm8: imm_i8(*imm) }
+                                    }
+                                }
                             }
-                            X86OperandType::Mem(_) => {
+                            X86OperandType::Mem(mem) => {
+                                dbg!("64bit mem");
+                                dbg!(&mem);
+                                let second = MemoryOperand::from_mem(mem);
+                                dbg!(second);
                                 todo!()
                             }
                             X86OperandType::Invalid => {
@@ -64,10 +132,23 @@ impl AdcInstruction {
                                 let to_add = Register32::new(*reg_id);
                                 AdcInstruction::R32Rm32 { target, to_add }
                             }
-                            X86OperandType::Imm(_) => {
-                                todo!()
+                            X86OperandType::Imm(imm) => {
+                                match OperandSize::from_capstone_size(operands[1].size) {
+                                    OperandSize::DoubleWord => {
+                                        AdcInstruction::Rm32imm32 { target, imm32: imm_i32(*imm) }
+                                    }
+                                    OperandSize::QuadWord |
+                                    OperandSize::Word => {
+                                        panic!("Unexpected constant size")
+                                    }
+                                    OperandSize::HalfWord => {
+                                        AdcInstruction::Rm32imm8 { target, imm8: imm_i8(*imm) }
+                                    }
+                                }
                             }
-                            X86OperandType::Mem(_) => {
+                            X86OperandType::Mem(mem) => {
+                                dbg!("32bit mem");
+                                dbg!(&mem);
                                 todo!()
                             }
                             X86OperandType::Invalid => {
@@ -82,10 +163,23 @@ impl AdcInstruction {
                                 let to_add = Register16::new(*reg_id);
                                 AdcInstruction::R16Rm16 { target, to_add }
                             }
-                            X86OperandType::Imm(_) => {
-                                todo!()
+                            X86OperandType::Imm(imm) => {
+                                match OperandSize::from_capstone_size(operands[1].size) {
+                                    OperandSize::QuadWord |
+                                    OperandSize::DoubleWord => {
+                                        panic!("Unexpected large constant")
+                                    }
+                                    OperandSize::Word => {
+                                        AdcInstruction::Rm16imm16 { target, imm16: imm_i16(*imm) }
+                                    }
+                                    OperandSize::HalfWord => {
+                                        AdcInstruction::Rm16imm8 { target, imm8: imm_i8(*imm) }
+                                    }
+                                }
                             }
-                            X86OperandType::Mem(_) => {
+                            X86OperandType::Mem(mem) => {
+                                dbg!("16bit mem");
+                                dbg!(&mem);
                                 todo!()
                             }
                             X86OperandType::Invalid => {
@@ -100,10 +194,13 @@ impl AdcInstruction {
                                 let to_add = Register8::new(*reg_id);
                                 AdcInstruction::R8Rm8 { target, to_add }
                             }
-                            X86OperandType::Imm(_) => {
-                                todo!()
+                            X86OperandType::Imm(imm) => {
+                                assert_eq!(OperandSize::from_capstone_size(operands[1].size), OperandSize::HalfWord);
+                                AdcInstruction::Rm8imm8 { target, imm8: imm_i8(*imm) }
                             }
-                            X86OperandType::Mem(_) => {
+                            X86OperandType::Mem(mem) => {
+                                dbg!("8bit mem");
+                                dbg!(&mem);
                                 todo!()
                             }
                             X86OperandType::Invalid => {
@@ -116,7 +213,9 @@ impl AdcInstruction {
             X86OperandType::Imm(_) => {
                 todo!()
             }
-            X86OperandType::Mem(_) => {
+            X86OperandType::Mem(mem) => {
+                dbg!("mem first");
+                dbg!(&mem);
                 todo!()
             }
             X86OperandType::Invalid => {
@@ -146,17 +245,25 @@ pub mod test {
             "adc ax, bx",
             "adc eax, ebx",
             "adc rax, rbx",
-            /*
             "adc al, -9",
             "adc ax, -9",
             "adc eax, -9",
             "adc rax, -9",
-            "adc byte ptr [rcx], -9",
-            "adc word ptr [rdx], -9",
-            "adc dword ptr [rax + rcx*8 - 5],  -9",
-            "adc qword ptr [rax],  -9",
-            "adc [rax], rbx",
-            */
+            //"adc byte ptr [rcx], -9",
+            //"adc word ptr [rdx], -99",
+            //"adc dword ptr [rax + rcx*8 - 5],  -999",
+            //"adc qword ptr [rax],  -9999",
+            //"adc word ptr [rdx], 257",
+            //"adc dword ptr [rax + rcx*8 - 5],  -900000",
+            //"adc qword ptr [rax],  900999",
+            //"adc byte ptr [rax],  bl",
+            //"adc word ptr [rax],  cx",
+            //"adc dword ptr [rax],  edx",
+            //"adc qword ptr [rax],  r9",
+            //"adc bl, byte ptr [rax]",
+            //"adc cx, word ptr [rax]",
+            //"adc edx, dword ptr [rax]",
+            "adc r9, qword ptr [rax]",
             );
             function_end_guard!();
         }
@@ -170,32 +277,48 @@ pub mod test {
         dbg!(&res);
 
 
-        assert!(res[1] == X86Instruction::Adc(AdcInstruction::R8Rm8 { target: MemoryOperandOrRegister8::Reg(Register8::AL), to_add: Register8::BL }));
-
-        assert!(res[2] == X86Instruction::Adc(AdcInstruction::R16Rm16 { target: MemoryOperandOrRegister16::Reg(Register16::AX), to_add: Register16::BX }));
-
-        assert!(res[3] == X86Instruction::Adc(AdcInstruction::R32Rm32 { target: MemoryOperandOrRegister32::Reg(Register32::EAX), to_add: Register32::EBX }));
-
-        assert!(res[4] == X86Instruction::Adc(AdcInstruction::R64Rm64 { target: MemoryOperandOrRegister64::Reg(Register64::RAX), to_add: Register64::RBX }));
+        //assert!(res[1] == X86Instruction::Adc(AdcInstruction::R8Rm8 { target: MemoryOperandOrRegister8::Reg(Register8::AL), to_add: Register8::BL }));
+        //assert!(res[2] == X86Instruction::Adc(AdcInstruction::R16Rm16 { target: MemoryOperandOrRegister16::Reg(Register16::AX), to_add: Register16::BX }));
+        //assert!(res[3] == X86Instruction::Adc(AdcInstruction::R32Rm32 { target: MemoryOperandOrRegister32::Reg(Register32::EAX), to_add: Register32::EBX }));
+        //assert!(res[4] == X86Instruction::Adc(AdcInstruction::R64Rm64 { target: MemoryOperandOrRegister64::Reg(Register64::RAX), to_add: Register64::RBX }));
     }
 
 
+
     #[test]
-    pub fn test_adc_r64rm64 () {
+    pub fn test_adc_r8rm8 () {
         #[no_mangle]
-        fn adc_r64rm64() {
+        fn adc_r8rm8 () {
             unsafe {
                 asm!(
-                "adc rax, rbx",
+                "adc al, bl",
                 );
                 function_end_guard!();
             }
         }
 
-        let raw_function_ptr = adc_r64rm64 as *const c_void;
+        let raw_function_ptr = adc_r8rm8 as *const c_void;
         let function_bytes = get_function_bytes(raw_function_ptr);
         let res = disassemble(function_bytes, raw_function_ptr as u64).unwrap();
-        assert!(res[1] == X86Instruction::Adc(AdcInstruction::R64Rm64 { target: MemoryOperandOrRegister64::Reg(Register64::RAX), to_add: Register64::RBX }));
+        assert!(res[1] == X86Instruction::Adc(AdcInstruction::R8Rm8 { target: MemoryOperandOrRegister8::Reg(Register8::AL), to_add: Register8::BL }));
+    }
+
+    #[test]
+    pub fn test_adc_r16rm16 () {
+        #[no_mangle]
+        fn adc_r16rm16() {
+            unsafe {
+                asm!(
+                "adc ax, bx",
+                );
+                function_end_guard!();
+            }
+        }
+
+        let raw_function_ptr = adc_r16rm16 as *const c_void;
+        let function_bytes = get_function_bytes(raw_function_ptr);
+        let res = disassemble(function_bytes, raw_function_ptr as u64).unwrap();
+        assert!(res[1] == X86Instruction::Adc(AdcInstruction::R16Rm16 { target: MemoryOperandOrRegister16::Reg(Register16::AX), to_add: Register16::BX }));
     }
 
     #[test]
@@ -214,6 +337,24 @@ pub mod test {
         let function_bytes = get_function_bytes(raw_function_ptr);
         let res = disassemble(function_bytes, raw_function_ptr as u64).unwrap();
         assert!(res[1] == X86Instruction::Adc(AdcInstruction::R32Rm32 { target: MemoryOperandOrRegister32::Reg(Register32::EAX), to_add: Register32::EBX }));
+    }
+
+    #[test]
+    pub fn test_adc_r64rm64 () {
+        #[no_mangle]
+        fn adc_r64rm64() {
+            unsafe {
+                asm!(
+                "adc rax, rbx",
+                );
+                function_end_guard!();
+            }
+        }
+
+        let raw_function_ptr = adc_r64rm64 as *const c_void;
+        let function_bytes = get_function_bytes(raw_function_ptr);
+        let res = disassemble(function_bytes, raw_function_ptr as u64).unwrap();
+        assert!(res[1] == X86Instruction::Adc(AdcInstruction::R64Rm64 { target: MemoryOperandOrRegister64::Reg(Register64::RAX), to_add: Register64::RBX }));
     }
 
 
